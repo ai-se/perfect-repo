278e2d6877d2ccdaa9360823cd9fe38e24d11346
Tim Menzies <tim.menzies@gmail.com>
2020-05-12 01:02:34 -0400

    asda

--- etc/help.sh
+++ etc/help.sh
@@ -1,5 +1,11 @@
 grep -E '^[a-zA-Z][^:]*:.*?## .*$' $* |
 sort | 
-awk 'BEGIN {FS = ":.*?## "}; \
-           {printf "\033[36m%-30s\033[0m %s\n", $1, $2}'
+awk 'BEGIN {
+   FS = ":.*?## "
+   C1=31
+   C2=36
+   printf("\n\033["C1"mmake [OPTION]\033[0m\n")
+   FMT="\033["C2"m%-15s\033[0m"
+} 
+{printf "\t"FMT ": %s\n", $1, $2}'
 
